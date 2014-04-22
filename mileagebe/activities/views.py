65810c2e5c751375dd5782d572f6935495c9b2a4
kahnjw <jarrod@yola.com>
2014-04-21 22:50:33 -0700

    More appropriate variable name

--- mileagebe/activities/views.py
+++ mileagebe/activities/views.py
@@ -6,9 +6,9 @@ from activities.service_clients import StravaServiceClient
 class StravaUser(APIView):
     def get(self, request):
         service_client = StravaServiceClient()
-        social_user = service_client.get_user_data(request.user)
+        strava_user = service_client.get_user_data(request.user)
 
-        return Response(social_user)
+        return Response(strava_user)
 
 
 class StravaActivities(APIView):
