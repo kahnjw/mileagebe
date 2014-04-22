633d909c7af1153c127754a997f6cbbb432531e2
kahnjw <jarrod@yola.com>
2014-04-22 00:09:50 -0700

    Remove comments from urls file.

--- mileagebe/mileagebe/urls.py
+++ mileagebe/mileagebe/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, url, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns(
     '',
@@ -13,15 +10,6 @@ urlpatterns = patterns(
     url(r'^api/v1/social-auth/',
         include('social.apps.django_app.urls', namespace='social')),
     url(r'^api/v1/', include('strava_client.urls'))
-    # Examples:
-    # url(r'^$', 'stompnet.views.home', name='home'),
-    # url(r'^stompnet/', include('stompnet.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
