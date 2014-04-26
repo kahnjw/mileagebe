d5400eebc11bc70bbd8df7deaaa0270683ea00cc
kahnjw <jarrod@yola.com>
2014-04-26 09:41:46 -0700

    Add user argument to Activity.objects.refresh() call

--- mileagebe/activities/tests.py
+++ mileagebe/activities/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from mock import patch
 
 from activities.models import Activity
+from extended_user.models import ExtendedUser
 
 
 APP_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -12,6 +13,8 @@ APP_PATH = os.path.abspath(os.path.dirname(__file__))
 
 class ActivityObjectsManagerTests(TestCase):
     def setUp(self):
+        self.user = ExtendedUser.objects.create_user(
+            username='Bob', password='Saget')
         self.raw_sample_data = open('%s/sample_activity_data.json' % APP_PATH)
         self.sample_data = json.load(self.raw_sample_data)
 
@@ -23,12 +26,12 @@ class ActivityObjectsManagerTests(TestCase):
         activities = Activity.objects.all()
         self.assertEqual(len(activities), 0)
 
-        Activity.objects.refresh()
+        Activity.objects.refresh(self.user)
         activities = Activity.objects.all()
         self.assertEqual(len(activities), 3)
 
     def test_refresh_creates_new_objects3(self):
-        Activity.objects.refresh()
+        Activity.objects.refresh(self.user)
         activities = Activity.objects.all()
         self.assertEqual(
             activities[0].strava_id, str(self.sample_data[0]['id']))
@@ -36,10 +39,10 @@ class ActivityObjectsManagerTests(TestCase):
             activities[2].strava_id, str(self.sample_data[2]['id']))
 
     def test_refresh_does_not_duplicate_activity_entries(self):
-        Activity.objects.refresh()
+        Activity.objects.refresh(self.user)
         activities = Activity.objects.all()
         self.assertEqual(len(activities), 3)
 
-        Activity.objects.refresh()
+        Activity.objects.refresh(self.user)
         activities = Activity.objects.all()
         self.assertEqual(len(activities), 3)
