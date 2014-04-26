30d165b415a70fc11c79af04a8123d113af2a40d
kahnjw <jarrod@yola.com>
2014-04-26 11:22:26 -0700

    Add related serializer fields

--- mileagebe/activities/serializers.py
+++ mileagebe/activities/serializers.py
@@ -6,5 +6,6 @@ from activities.models import Activity
 class ActivitySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Activity
-        fields = ('strava_id', 'location_city', 'distance', 'moving_time',
-                  'max_speed', 'average_speed', 'total_elevation_gain', 'user')
+        fields = ('url', 'strava_id', 'location_city', 'distance', 'gear',
+                  'max_speed', 'average_speed', 'total_elevation_gain', 'user',
+                  'moving_time')
--- mileagebe/gear/serializers.py
+++ mileagebe/gear/serializers.py
@@ -4,6 +4,10 @@ from gear.models import Gear
 
 
 class GearSerializer(serializers.HyperlinkedModelSerializer):
+    total_mileage = serializers.Field(source='get_mileage')
+    miles_left = serializers.Field(source='get_miles_left')
+
     class Meta:
         model = Gear
-        fields = ('name', 'untracked_mileage', 'lifetime', 'user')
+        fields = ('name', 'untracked_mileage', 'lifetime', 'user', 'url',
+                  'activities', 'total_mileage', 'miles_left')
