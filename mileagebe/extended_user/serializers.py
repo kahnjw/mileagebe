8eee314c180bdc6b8f3efbcbcbd50a67ad492b4d
kahnjw <jarrod@yola.com>
2014-04-26 23:35:03 -0700

    Add auth endpoint

--- mileagebe/extended_user/serializers.py
+++ mileagebe/extended_user/serializers.py
@@ -1,12 +1,17 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework import serializers
 
 from extended_user.models import ExtendedUser
 
 
-class ExtendedUserSerializer(HyperlinkedModelSerializer):
+class ExtendedUserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = ExtendedUser
         fields = ('url', 'username', 'password', 'gear', 'activities')
         write_only_fields = ('password', )
         read_only_fields = ('gear', 'activities')
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
