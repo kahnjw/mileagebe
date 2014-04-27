fb234199c1ef54dfe95690a1ad55e595da6c4db3
kahnjw <jarrod@yola.com>
2014-04-26 23:34:50 -0700

    Add nicely formatted dates and times to activities endpoint

--- mileagebe/activities/models.py
+++ mileagebe/activities/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 from strava_client.service_clients import StravaServiceClient
@@ -11,15 +13,21 @@ class ActivityManager(models.Manager):
         return [self._save_activity(user, raw_act) for raw_act in raw_acts]
 
     def _save_activity(self, user, raw_act):
+        import pprint
+        pprint.pprint(raw_act)
         activity, created = Activity.objects.get_or_create(
             user=user, strava_id=raw_act['id'])
 
         if created:
-            activity.distance = raw_act['distance']
-            activity.moving_time = raw_act['moving_time']
-            activity.max_speed = raw_act['max_speed']
-            activity.average_speed = raw_act['average_speed']
-            activity.total_elevation_gain = raw_act['total_elevation_gain']
+            activity.name = raw_act.get('name')
+            activity.activity_type = raw_act.get('type')
+            activity.location_city = raw_act.get('location_city')
+            activity.distance = raw_act.get('distance')
+            activity.moving_time = raw_act.get('moving_time')
+            activity.max_speed = raw_act.get('max_speed')
+            activity.average_speed = raw_act.get('average_speed')
+            activity.total_elevation_gain = raw_act.get('total_elevation_gain')
+            activity.start_date_time = raw_act.get('start_date_local')
             activity.save()
 
         return activity
@@ -31,6 +39,8 @@ class Activity(models.Model):
     user = models.ForeignKey(
         'extended_user.ExtendedUser', related_name='activities')
 
+    name = models.CharField(max_length=1024, null=True, blank=True)
+    activity_type = models.CharField(max_length=255, null=True, blank=True)
     strava_id = models.CharField(max_length=255, null=True, blank=True)
     location_city = models.CharField(max_length=255)
     distance = models.FloatField(default=0)
@@ -38,3 +48,12 @@ class Activity(models.Model):
     max_speed = models.FloatField(default=0)
     average_speed = models.FloatField(default=0)
     total_elevation_gain = models.FloatField(default=0)
+    start_date_time = models.CharField(max_length=255)
+
+    def get_nice_moving_time(self):
+        return str(datetime.timedelta(seconds=self.moving_time))
+
+    def get_nice_start_date(self):
+        time = datetime.datetime.strptime(
+            self.start_date_time, '%Y-%m-%dT%H:%M:%SZ')
+        return '%s/%s/%s' % (time.month, time.day, time.year)
