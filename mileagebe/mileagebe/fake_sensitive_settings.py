2148e32092d553013558dcb342226ba6304c662c
kahnjw <jarrod@yola.com>
2014-05-09 19:56:38 -0700

    Add static dir to fake settings.py

--- mileagebe/mileagebe/fake_sensitive_settings.py
+++ mileagebe/mileagebe/fake_sensitive_settings.py
@@ -2,6 +2,7 @@ SENSITIVE_SETTINGS = {
     "SECRET_KEY": "fake_key",
     "SOCIAL_AUTH_STRAVA_KEY": 1234,
     "SOCIAL_AUTH_STRAVA_SECRET": "fake_secret",
+    "STATIC_ROOT": '/static',
     "DB_CONFIG": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": "db.sqlite3"
